1c72ab92a918b5e02cdd1b626d4c654b36ef6cdc
James Hall <halljames631@gmail.com>
2019-12-20 13:36:41 -0500

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,69 @@
+require 'pry'
+class TicTacToe
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ]
+
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+ 
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+ 
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+ 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(key, token) 
+    @board[key] = token
+  end
+
+  def position_taken?(index)
+    if @board[index] != " "
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(index)
+    if position_taken?(index) || index > 8
+      false
+    else
+      true
+    end
+  end
+
+  def turn
+    input = gets.to_i
+    index = input_to_index(input)
+    token = current_player
+    if valid_move?(input)
+      move(index, token) 
+    else
+      turn
+    end
+    display_board
+  end
+end
